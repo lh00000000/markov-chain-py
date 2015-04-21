e48d5c7384d8e3a838c2cf722f8dded9a02f9b27
luming hao <lh00000000@gmail.com>
2015-04-21 12:31:23 -0400

    added bigram markov

--- markov.py
+++ markov.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from itertools import chain
 import random
 
-class MarkovCharacter(object):
+class TrigramMarkov(object):
 
     def __init__(self, filepath):
         self.knowledge = defaultdict(list)
@@ -52,10 +52,61 @@ class MarkovCharacter(object):
         else:
             return phrase.strip()
 
-def main():
-    mk = MarkovCharacter("tswift-corpus.txt")
-    mk.train("birds-rights-activist-corpus.txt")
+class BigramMarkov(object):
 
+    def __init__(self, filepath):
+        self.knowledge = defaultdict(list)
+        self.train(filepath)
+
+    def train(self, filepath):
+        def text_to_knowledge(text):
+            def tokenize_text(text):
+                def tokenize_line(line):
+                    line_tokens = [w.strip() for w in line.split() if w.strip() != '']
+                    return line_tokens
+                tokens = [tokenize_line(line) for line in text]
+                return list(chain.from_iterable(tokens))
+
+            def yield_bigrams():
+                if len(tokens) < 2:
+                    return
+                for i in range(len(tokens) - 2):
+                    yield (tokens[i], tokens[i + 1])
+
+            tokens = tokenize_text(text)
+            text_knowledge = defaultdict(list)
+            for w1, w2 in yield_bigrams():
+                text_knowledge[w1].append(w2)
+            return text_knowledge
+
+        def append_new_knowledge(new_knowledge):
+            for gram in new_knowledge.items():
+                self.knowledge[gram[0]].append(gram[1][0])
+            
+        with open(filepath) as f:
+            text = [line for line in f]
+        append_new_knowledge(text_to_knowledge(text))
+
+    def generate_phrase(self, min_chars=100, max_chars=140):
+        random_bigram_key = random.choice(self.knowledge.keys())
+
+        w1, w2 = random_bigram_key, random.choice(self.knowledge[random_bigram_key])
+        phrase = '  '
+
+        while phrase[-2] not in '.!?' and len(phrase) < max_chars:
+            phrase += '%s ' %(w1)
+            w1, w2 = w2, random.choice(self.knowledge[w2])
+
+        if (len(phrase) < min_chars):
+            return self.generate_phrase(min_chars, max_chars)
+        else:
+            return phrase.strip()
+
+def main():
+    mk = TrigramMarkov("corpuses/birds-rights-activist-corpus.txt")
+    mk.train("corpuses/tswift-corpus.txt")
+    mk.train("corpuses/cocoabean-corpus.txt")
+    
     print mk.generate_phrase()
 
 
